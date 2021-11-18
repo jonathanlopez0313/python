edcc38ec91b119f98d85967dbcf14763b7911781
jonathanlopez0313 <94642795+jonathanlopez0313@users.noreply.github.com>
2021-11-18 12:12:25 -0800

    Add files via upload

---
+++ martes.py
@@ -0,0 +1,41 @@
+# Tuesday Nov. 16
+
+y = 6
+x = [1,2]
+
+# create list
+
+print("---> what I am carrying")
+
+inventory = ["bike", "warm-up pants", "car","lunch" ]
+
+# print variables
+
+print(y)
+print(x)
+
+print(inventory)
+print(inventory[2])
+
+# first loop
+
+print("---> regular loop")
+
+count = 0
+
+while count < 4:
+    print(inventory[count])
+    count = count + 1
+
+# second loop
+
+print("---> backward loop")
+
+count = 3
+
+while count < 0:
+    print(inventory[count])
+    count = count - 1
+# for loop
+
+print("The inventory for loop")
